14cf7f86ea7bf3d0863a8cb19ab492095f2d49ad
NoiseHY <congnamhy1@gmail.com>
2023-12-19 15:38:47 +0700

    Sửa sản phẩm thành công !!

--- Font-end/Trang quản trị/Quản trị/Product/Product.js
+++ Font-end/Trang quản trị/Quản trị/Product/Product.js
@@ -95,7 +95,7 @@ app.controller('productController', function ($scope, $http) {
       })
       .catch(function (error) {
         // Xử lý khi có lỗi
-        alert('Đã xảy ra lỗi khi tạo sản phẩm!');
+        alert('Đã xảy ra lỗi khi thêm sản phẩm!');
         console.error('Lỗi khi tạo sản phẩm:', error);
       });
   };
@@ -111,42 +111,62 @@ app.controller('productController', function ($scope, $http) {
     $scope.product.productDescription = $scope.selectedProduct.mota;
     $scope.product.productQuantity = $scope.selectedProduct.soLuong;
     $scope.product.productPrice = $scope.selectedProduct.dongia;
-    
-    // Gọi API để lấy tên thể loại
-    $http.get('https://localhost:7117/api/category/GetNameCategoryByID?id=' + $scope.selectedProduct.maTL)
-      .then(function (response) {
-        if (Array.isArray(response.data) && response.data.length > 0) {
-          $scope.selectedCategory = response.data[0]; // Lấy giá trị từ phần tử đầu tiên trong mảng
-        } else {
-          console.error('Không có dữ liệu thể loại trả về.');
-        }
-      })
-      .catch(function (error) {
-        console.error('Lỗi khi lấy tên thể loại:', error);
-      });
+
+    // $scope.selectedCategory = "Thịt 12312321";
+    // // Gọi API để lấy tên thể loại
+    // $http.get('https://localhost:7117/api/category/GetNameCategoryByID?id=' + $scope.selectedProduct.maTL)
+    //   .then(function (response) {
+    //     if (Array.isArray(response.data) && response.data.length > 0) {
+    //       $scope.selectedCategory = response.data[0].tenLoai;
+    //     } else {
+    //       alert('Không có dữ liệu thể loại trả về.');
+    //     }
+    //   })
+    //   .catch(function (error) {
+    //     alert('Lỗi khi lấy tên thể loại:', error);
+    //   });
   };
 
   $scope.updateProduct = function () {
     var productData = {
-      maSP: $scope.selectedProduct.maSP,
-      tenSP: $scope.selectedProduct.tenSP,
-      mota: $scope.selectedProduct.mota,
-      soLuong: $scope.selectedProduct.soLuong,
-      dongia: $scope.selectedProduct.dongia
+      maSP: $scope.product.id,
+      tenSP: $scope.product.productName,
+      mota: $scope.product.productDescription,
+      soLuong: $scope.product.productQuantity,
+      dongia: $scope.product.productPrice,
+      maTL: $scope.selectedCategory ? $scope.selectedCategory.maLoai : null,
     };
 
     $http.put('https://localhost:7117/api/product/Update', productData)
       .then(function (response) {
-        // Xử lý khi sửa sản phẩm thành công
         alert('Sửa sản phẩm thành công!');
-        console.log(response.data); // log kết quả từ API
+        console.log(response.data);
       })
       .catch(function (error) {
-        // Xử lý khi có lỗi
         alert('Đã xảy ra lỗi khi sửa sản phẩm!');
         console.error('Lỗi khi sửa sản phẩm:', error);
       });
   };
+
+  $scope.confirmDelete = function (productId) {
+    if (confirm('Bạn có chắc chắn muốn xóa sản phẩm này không?')) {
+      debugger;
+      $scope.deleteProduct(productId);
+
+    }
+  };
+
+  $scope.deleteProduct = function (productId) {
+    $http.delete('https://localhost:7117/api/product/Delete/' + productId)
+      .then(function (response) {
+        alert('Xóa thành công sản phẩm !');
+        console.log(response.data);
+        // Tải lại danh sách sản phẩm sau khi xóa thành công
+        $scope.getProductList();
+      })
+      .catch(function (error) {
+        alert('Đã xảy ra lỗi khi xóa sản phẩm!');
+        console.error('Lỗi khi xóa sản phẩm:', error);
+      });
+  };
 });
-
-
